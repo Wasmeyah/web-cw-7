5d9b9925e24bae1e25c420cab71713aa50771cf1
Wasmeyah <wasmya2013@gmail.com>
2021-01-05 20:55:47 +0300

    alll doneeeee :)

--- question1.js
+++ question1.js
@@ -1,1 +1,14 @@
 // قم بحل السؤال الأول هنا
+let pets =[`cat`,`bird`,`tuutle`,`dog`,`lion`];
+pets.pop();
+pets.push(`Zepra`)
+console.log(pets)
+
+let Cat = {
+    name:'Duke',
+    age:"3 years old",
+    type:"perisen cat",
+    emoji:'👑'
+
+}
+console.log(Cat.age)
